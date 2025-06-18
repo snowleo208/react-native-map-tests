4b553b8ea8d8e54bb6e271f8e464d6261d8ec030
snowleo208 <13027432+snowleo208@users.noreply.github.com>
2025-06-18 19:40:42 +0100

    fix: unit tests

--- app/__mocks__/react-native-maps.tsx
+++ app/__mocks__/react-native-maps.tsx
@@ -1,23 +1,25 @@
 import { forwardRef, useImperativeHandle, useLayoutEffect } from 'react';
-import { View } from 'react-native';
+import { LayoutChangeEvent, View } from 'react-native';
 import { MapMarkerProps, MapViewProps } from 'react-native-maps';
 
 export const mockMapRef = {
   animateToRegion: jest.fn(),
 };
 
-const MapView = forwardRef(({ children, onMapReady, ...props }: MapViewProps, ref: React.Ref<unknown>) => {
+const MapView = forwardRef(({ children, onLayout, ...props }: MapViewProps, ref: React.Ref<unknown>) => {
   useImperativeHandle(ref, () => mockMapRef);
 
   useLayoutEffect(() => {
-    if (!onMapReady) {
-      return;
+    if (onLayout) {
+      onLayout({
+        nativeEvent: {
+          layout: { x: 0, y: 0, width: 300, height: 400 },
+        },
+      } as unknown as LayoutChangeEvent);
     }
 
-    setTimeout(() => {
-      onMapReady();
-    }, 200);
-  }, [onMapReady]);
+  }, [onLayout]);
+
 
   return <View testID="mock-map-view"  {...props}>{children}</View>;
 });
